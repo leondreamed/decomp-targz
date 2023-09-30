754bcb8e5249b7b6a6bd248bc6eea06a7a4952e6
Leon Si <leon@leondreamed.com>
2023-09-30 17:22:30 -0400

    fix: tests

--- index.js
+++ index.js
@@ -1,26 +1,27 @@
-'use strict';
-const zlib = require('zlib');
-const decompressTar = require('decompress-tar');
-const fileType = require('file-type');
-const isStream = require('is-stream');
+import zlib from "zlib";
+import decompressTar from "decomp-tar";
+import fileType from "file-type";
+import { isStream } from "is-stream";
 
-module.exports = () => input => {
-	if (!Buffer.isBuffer(input) && !isStream(input)) {
-		return Promise.reject(new TypeError(`Expected a Buffer or Stream, got ${typeof input}`));
-	}
+export default () => (input) => {
+  if (!Buffer.isBuffer(input) && !isStream(input)) {
+    return Promise.reject(
+      new TypeError(`Expected a Buffer or Stream, got ${typeof input}`)
+    );
+  }
 
-	if (Buffer.isBuffer(input) && (!fileType(input) || fileType(input).ext !== 'gz')) {
-		return Promise.resolve([]);
-	}
+  if (Buffer.isBuffer(input) && fileType(input)?.ext !== "gz") {
+    return Promise.resolve([]);
+  }
 
-	const unzip = zlib.createGunzip();
-	const result = decompressTar()(unzip);
+  const unzip = zlib.createGunzip();
+  const result = decompressTar()(unzip);
 
-	if (Buffer.isBuffer(input)) {
-		unzip.end(input);
-	} else {
-		input.pipe(unzip);
-	}
+  if (Buffer.isBuffer(input)) {
+    unzip.end(input);
+  } else {
+    input.pipe(unzip);
+  }
 
-	return result;
+  return result;
 };
--- test.js
+++ test.js
@@ -1,45 +1,52 @@
-import fs from 'fs';
-import path from 'path';
-import isJpg from 'is-jpg';
-import pify from 'pify';
-import test from 'ava';
-import m from '.';
+import fs from "fs";
+import isJpg from "is-jpg";
+import test from "ava";
+import m from "./index.js";
+import { join, filename } from "desm";
 
-const fsP = pify(fs);
+test("extract file", async (t) => {
+  const buf = await fs.promises.readFile(
+    join(import.meta.url, "fixtures", "file.tar.gz")
+  );
+  const files = await m()(buf);
 
-test('extract file', async t => {
-	const buf = await fsP.readFile(path.join(__dirname, 'fixtures', 'file.tar.gz'));
-	const files = await m()(buf);
-
-	t.is(files[0].path, 'test.jpg');
-	t.true(isJpg(files[0].data));
+  t.is(files[0].path, "test.jpg");
+  t.true(isJpg(files[0].data));
 });
 
-test('extract file using streams', async t => {
-	const stream = fs.createReadStream(path.join(__dirname, 'fixtures', 'file.tar.gz'));
-	const files = await m()(stream);
+test("extract file using streams", async (t) => {
+  const stream = fs.createReadStream(
+    join(import.meta.url, "fixtures", "file.tar.gz")
+  );
+  const files = await m()(stream);
 
-	t.is(files[0].path, 'test.jpg');
-	t.true(isJpg(files[0].data));
+  t.is(files[0].path, "test.jpg");
+  t.true(isJpg(files[0].data));
 });
 
-test('return empty array if non-valid file is supplied', async t => {
-	const buf = await fsP.readFile(__filename);
-	const files = await m()(buf);
+test("return empty array if non-valid file is supplied", async (t) => {
+  const buf = await fs.promises.readFile(filename(import.meta.url));
+  const files = await m()(buf);
 
-	t.is(files.length, 0);
+  t.is(files.length, 0);
 });
 
-test('throw on wrong input', async t => {
-	await t.throws(m()('foo'), 'Expected a Buffer or Stream, got string');
+test("throw on wrong input", async (t) => {
+  await t.throwsAsync(() => m()("foo"), {
+    message: "Expected a Buffer or Stream, got string",
+  });
 });
 
 // Don't run this test on Node.js v4
 // https://github.com/nodejs/node/commit/80169b1f0a5f944b99e82a409536dea426c992f3
-if (!process.version.startsWith('v4')) {
-	test('handle gzip error', async t => {
-		const buf = await fsP.readFile(path.join(__dirname, 'fixtures', 'fail.tar.gz'));
-		const err = await t.throws(m()(buf), 'unexpected end of file');
-		t.is(err.code, 'Z_BUF_ERROR');
-	});
+if (!process.version.startsWith("v4")) {
+  test("handle gzip error", async (t) => {
+    const buf = await fs.promises.readFile(
+      join(import.meta.url, "fixtures", "fail.tar.gz")
+    );
+    const err = await t.throwsAsync(() => m()(buf), {
+      message: "unexpected end of file",
+    });
+    t.is(err.code, "Z_BUF_ERROR");
+  });
 }
